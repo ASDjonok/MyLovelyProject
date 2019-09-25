e72cab8ceffe58ba984a3d755bb9b7cb2a8e2bc9
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-09-25 13:28:28 +0300

    Initial commit

---
+++ src/Lab3.java
@@ -0,0 +1,8 @@
+public class Lab3 {
+    public static void main(String[] args) {
+        String s = new String("ASD");
+        s = s + "B";
+        StringBuilder stringBuilder = new StringBuilder("ASD");
+        stringBuilder.append("B");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner =
+                new Scanner(System.in);
+        String s = scanner.nextLine();
+
+        long a = scanner.nextLong();
+        System.out.printf("a = " + a);
+    }
+}
