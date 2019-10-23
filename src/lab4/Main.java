9f89a330480305e524e303f7a72c63e792578c6d
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-10-23 14:14:38 +0300

    add lambdas and anonymous class to lab4

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -1,6 +1,7 @@
 package lab4;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     int myField;
@@ -16,6 +17,29 @@ public class Main {
         Arrays.sort(myObjects, new MyComparator());
         System.out.println(Arrays.toString(myObjects));
         Arrays.sort(myObjects, new MyComparator2());
+//        Arrays.sort(new String[]{"123", "ASD"}, new MyComparator2());
+
+
+        Arrays.sort(myObjects, new Comparator<MyClass>() {
+            @Override
+            public int compare(MyClass o1, MyClass o2) {
+                return o1.getField1() - o2.getField1();
+            }
+        });
+
+        Arrays.sort(myObjects, (o1, o2) ->
+                o1.getField1() - o2.getField1());
+
+        Arrays.sort(myObjects,
+                Comparator.comparingInt(MyClass::getField1));
+
+        Arrays.sort(myObjects, (o1, o2) ->
+                {
+                    System.out.println(":p");
+                    return o1.getField1() - o2.getField1();
+                }
+        );
+
         System.out.println(Arrays.toString(myObjects));
         /*myObjects[0].setField1(1, "1111");
         myObjects[0].setField2(2);
